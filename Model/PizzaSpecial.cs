bbf13eab566171f6b3bd66479bb9a46412a2a609
sochima-ifedikwa <ifedikwasochima@gmail.com>
2026-03-24 21:20:09 -0500

    first commit

---
+++ Model/PizzaSpecial.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazingPizza
+{
+    /// <summary>
+    /// Represents a pre-configured template for a pizza a user can order
+    /// </summary>
+    public class PizzaSpecial
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal BasePrice { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string GetFormattedBasePrice()
+        {
+            return BasePrice.ToString("0.00");
+        }
+    }
+}
